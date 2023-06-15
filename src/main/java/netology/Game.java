564f769dc6729dcfa8f0d870e8c24fb89206580a
Pers test <qa61test@gmail.com>
2023-06-15 16:08:08 +0400

    Map tested

--- src/main/java/netology/Game.java
+++ src/main/java/netology/Game.java
@@ -1,36 +1,40 @@
 package netology;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Game {
 
-    ArrayList<Player> playersList = new ArrayList<>();
+    HashMap<String, Integer> playersList = new HashMap<>();
 
     public void registration(Player player) {
-        playersList.add(player);
+        playersList.put(player.getName(),player.getStrength());
     }
 
     public int round(String playerName1, String playerName2) {
-        Player player1 = null;
-        Player player2 = null;
-        for (Player player : playersList) {
-            if (player.getName().equals(playerName1)) {
-                player1 = player;
-            }
-            if (player.getName().equals(playerName2)) {
-                player2 = player;
+        String player1[] = null;
+        String player2[] = null;
+        for ( String key : playersList.keySet()) {
+            if (playersList.containsKey(playerName1)) {
+                player1 = new String[]{"Зарегистрирован"};
             }
         }
+        for (String key : playersList.keySet()) {
+            if (playersList.containsKey(playerName2)) {
+                player2 = new String[]{"Зарегистрирован"};
+            }
+        }
+
         if (player1 == null) {
             throw new NotFoundException("Игрок с именем " + playerName1 + " не зарегистрирован");
         }
         if (player2 == null) {
             throw new NotFoundException("Игрок с именем " + playerName2 + " не зарегистрирован");
         }
-        if (player1.getStrength() > player2.getStrength()) {
+        if (playersList.get(playerName1) > playersList.get(playerName2)) {
             return 1;
         }
-        if (player1.getStrength() < player2.getStrength()) {
+        if (playersList.get(playerName1) < playersList.get(playerName2)) {
             return 2;
         }
         return 0;
