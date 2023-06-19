36d34d956fb215e73c24ffccc59867009659f5f3
Pers test <qa61test@gmail.com>
2023-06-19 22:28:35 +0400

    Game code fixed

--- src/main/java/netology/Game.java
+++ src/main/java/netology/Game.java
@@ -12,23 +12,12 @@ public class Game {
     }
 
     public int round(String playerName1, String playerName2) {
-        String player1[] = null;
-        String player2[] = null;
-        for ( String key : playersList.keySet()) {
-            if (playersList.containsKey(playerName1)) {
-                player1 = new String[]{"Зарегистрирован"};
-            }
-        }
-        for (String key : playersList.keySet()) {
-            if (playersList.containsKey(playerName2)) {
-                player2 = new String[]{"Зарегистрирован"};
-            }
-        }
 
-        if (player1 == null) {
+
+        if (!playersList.containsKey(playerName1)) {
             throw new NotFoundException("Игрок с именем " + playerName1 + " не зарегистрирован");
         }
-        if (player2 == null) {
+        if (!playersList.containsKey(playerName2)) {
             throw new NotFoundException("Игрок с именем " + playerName2 + " не зарегистрирован");
         }
         if (playersList.get(playerName1) > playersList.get(playerName2)) {
